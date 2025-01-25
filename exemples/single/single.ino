2f3a1fc75f71e31b3538fdbae36178a1c0b683d0
LrFranca <117331016+LrFranca@users.noreply.github.com>
2025-01-25 17:46:32 -0300

    Arduino.ino

--- exemples/single/single.ino
+++ exemples/single/single.ino
@@ -1,5 +1,8 @@
 #include <DIYcolorsensor.h>
 
+const unsigned char PS_16 = (1 << ADPS2);
+const unsigned char PS_128 = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
 ColorSensor sensor(6, 7, 8, A0);
 
 void setup() {
