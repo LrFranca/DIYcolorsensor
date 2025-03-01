c8a95db40402cb379ee2622e0ef3adbe488dae7a
LrFranca <117331016+LrFranca@users.noreply.github.com>
2025-03-01 20:09:56 -0300

    Update Arduino.ino

--- exemples/single/Arduino.ino
+++ exemples/single/Arduino.ino
@@ -1,5 +1,5 @@
-#include <DIYcolorsensor.h>
 
+#include <DIYcolorsensor.h>
 const unsigned char PS_16 = (1 << ADPS2);
 const unsigned char PS_128 = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 
@@ -7,6 +7,14 @@ ColorSensor sensor(6, 7, 8, A0);
 
 void setup() {
   Serial.begin(9600);
+  ADCSRA &= ~PS_128;
+  ADCSRA |= PS_16;
+  
+  /*
+  In case you wanna set those values manually:
+  sensor.setBlackMin(0, 0, 0); 
+  sensor.setBlackMax(0, 0, 0);
+  */
 }
 void loop() {
   sensor.getColor();
